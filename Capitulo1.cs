98ad842286781ddf9ebcd815e85cd211aa98fff6
prof-santiago <santiago.c@edu.sc.senai.br>
2026-06-30 14:58:13 -0300

    Adicionadas pastas

--- Capitulo1.cs
+++ Capitulo1.cs
@@ -3,6 +3,6 @@ public static class Capitulo1
 {
     public static void FunçãoTeste1()
     {
-        Console.WriteLine("Teste Capitulo 3");  
+        Console.WriteLine("Teste Capitulo 1");  
     }
 }
