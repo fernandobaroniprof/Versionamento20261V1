6c54857378a30587d3354439ba9fb42f7d2b6d65
VITOR CANDIDO ALVES <vitor_alves152@estudante.sesisenai.org.br>
2026-07-01 15:37:32 -0300

    Código adicionado

--- Capitulo2.cs
+++ Capitulo2.cs
@@ -1,8 +1,38 @@
 // ===== SQUAD B =====
 public static class Capitulo2
 {
+    int escolha;
+    
     public static void FunçãoTeste2()
     {
-        Console.WriteLine("Teste Capitulo 2");
+        Console.WriteLine("Qual seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("");
+        escolha = int.Parse(Console.ReadLine());
+
+
+        switch (escolha)
+        {
+          case "1":
+
+           Console.WriteLine("");
+           Console.WriteLine("");
+          break;
+
+          case "2":
+
+           Console.WriteLine("");
+           Console.WriteLine("");
+          break;          
+
+          default:
+
+           Console.WriteLine("");
+           escolha = int.Parse(Console.ReadLine());
+          break;
+
+
+        }
     }
 }
