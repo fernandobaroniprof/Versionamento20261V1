638561899a3732f0785d7ff367a2613b4eff78b9
VOLNEI FIDÉLIS NETO <volnei_fidelis-ne@estudante.sesisenai.org.br>
2026-07-01 16:36:13 -0300

    HISTORIA Equipe 3

--- Capitulo3.cs
+++ Capitulo3.cs
@@ -9,4 +9,12 @@ public static class Capitulo3
 
         Console.WriteLine("\n No dia da prova final, o protagonista colocou em prática tudo o que aprendeu durante o curso e com a ajuda de seus amigos. Apesar do nervosismo, conseguiu criar o jogo 3D dentro do tempo e impressionou os avaliadores. Com isso, alcançou necessária e concluiu a primeira parte do curso, pronto para enfrentar novos desafios.");
     }
+    // ===== SQUAD C =====
+
+
+    public static void FunçãoTeste3()
+    {
+        Console.WriteLine("Após terminar a penúltima prova do curso,o protagonista se vê em uma última situação para alcançar a nota final da primeira parte do seu curso, tendo em vista que a prova final era criar um jogo 3D em apenas um dia, e ele tem apenas 3 dias para se preparar para o último desafio. Então ele pediu ajuda ao seu grupo de amigos feitos durante a primeira etapa do curso, cada um dos amigos ensinou a ele uma habilidade diferente para ele fazer a prova final com mais facilidade. Então o dia da prova final, o protagonista estava um pouco nervoso pois não podia falhar.");
+    }
+
 }
